78e80db141b7f507b38bdcbea6f7bfb59f12e811
gonghao <gonghao@yunke.com>
2016-11-29 18:06:16 +0800

    Initial Commit

---
+++ NetworkRequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkRequest
+//
+//  Created by 龚浩 on 16/11/29.
+//  Copyright © 2016年 龚浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
